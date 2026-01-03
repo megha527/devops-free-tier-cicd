45c5d940eb56b4a4383f55da5263acaea8a94820
Megha <kumarimegha927@gmail.com>
2026-01-03 07:05:14 +0530

    Day 4: Terraform setup via Jenkins

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,28 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "ap-south-1"
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "free-tier-web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
