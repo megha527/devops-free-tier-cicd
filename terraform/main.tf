579cbdcd4da50860fdf8753f57e16cd798348152
Megha <kumarimegha927@gmail.com>
2026-01-04 08:27:36 +0530

    Day 5: Terraform apply added EC2 provisioning

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# Security Group
 resource "aws_security_group" "web_sg" {
   name = "free-tier-web-sg"
 
@@ -24,5 +25,17 @@ resource "aws_security_group" "web_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "app_server" {
+  ami             = "ami-02b8269d5e85954ef" # Ubuntu 24.04 LTS
+  instance_type   = "t2.micro"
+  key_name        = "jenkins-key" # Must match your AWS key
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "free-tier-app-server"
   }
 }
